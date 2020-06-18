6f9198c09a1648ed8bd11b7c1bdec8b47df22032
Gincral <ssssrxdd@gmail.com>
2020-06-18 18:40:14 -0400

    .

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => { 
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Origin', '*');
     next(); 
 });
 
